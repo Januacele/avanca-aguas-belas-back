70df4d928703d1e6368cc60ad8553fcb0d4c110b
Januacele <januacelevieira@hotmail.com>
2022-09-29 00:06:41 -0300

    :boom: fix: fixing userSchema function

--- src/schemas/authSchema.ts
+++ src/schemas/authSchema.ts
@@ -1,9 +1,14 @@
 import Joi from "joi";
 
+// eslint-disable-next-line no-useless-escape
+const regexCPF = /[0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}/;
 
 export const userSchema = Joi.object({
     email: Joi.string().email().required(),
-    password: Joi.string().min(10).required()
+    password: Joi.string().min(10).required(),
+    name: Joi.string().required(),
+    cpf: Joi.string().required().pattern(regexCPF),
+    secretary: Joi.string().required()
 });
 
 export const loginSchema = Joi.object({
