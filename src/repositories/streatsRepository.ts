bf59b3edaa6f6239341664c3268ffc3396fd2e99
Januacele <januacelevieira@hotmail.com>
2022-09-30 18:28:36 -0300

    :construction: feat: insert function findStreats streatsRepository

--- src/repositories/streatsRepository.ts
+++ src/repositories/streatsRepository.ts
@@ -5,32 +5,36 @@ export async function insertStreat(streats: IRegisterStreatsNames) {
     return prisma.streats.create({
      data: streats
     });
-  }
+}
 
-  export async function findStreatById(id: number) {
+export async function findStreats() {
+    return prisma.streats.findMany();
+}
+
+export async function findStreatById(id: number) {
     return prisma.streats.findUnique({
       where: {
         id,
       },
     });
-  }
+}
 
-  export async function updateStreatName(id: number, streats: IRegisterStreatsNames){
+export async function updateStreatName(id: number, streats: IRegisterStreatsNames){
     return prisma.streats.update({
       where: {
         id,
       },
       data: streats
     });
-  }
+}
 
-  export async function deleteStreatName(id: number){
+export async function deleteStreatName(id: number){
     return prisma.streats.delete({
       where: {
         id
       }
-    })
-  }
+    });
+}
 
 
 
