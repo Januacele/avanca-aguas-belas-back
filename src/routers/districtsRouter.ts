066e7dc3f690f2ba4ae54eb59d40b50d09ef279a
Januacele <januacelevieira@hotmail.com>
2022-10-03 23:44:20 -0300

    :construction: feature: add jwt middleware

--- src/routers/districtsRouter.ts
+++ src/routers/districtsRouter.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { validateSchemaMiddleware } from "../middlewares/validateSchemaMiddleware.js";
+import { jwtAutenticateMiddleware } from "../middlewares/jwtMiddleware.js";
 import * as districtNameSchema from "../schemas/districtsSchema.js";
 import * as districtsController from "../controllers/districtsController.js";
 
 const districtsRouter = Router();
+
+districtsRouter.use(jwtAutenticateMiddleware);
 
 districtsRouter.post("/district", 
 validateSchemaMiddleware(districtNameSchema.districtNameSchema),
