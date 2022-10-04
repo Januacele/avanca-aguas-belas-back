15f4f51516dcdbd5b30f72b46e47008fec0eb8dd
Januacele <januacelevieira@hotmail.com>
2022-10-04 02:07:37 -0300

    :construction: feature: add fucntion getStreatName - controller

--- src/controllers/streatsController.ts
+++ src/controllers/streatsController.ts
@@ -7,4 +7,10 @@ export async function creatStreatName(req: Request, res: Response) {
   const createDistrictName: IStreats = req.body;
   await streatService.createStreatName(createDistrictName);
   res.status(201).send("District added");
-}
+};
+
+
+export async function getStreatName(req: Request, res: Response) {
+  const getAllStreats = await streatService.getStreatsName();
+  res.status(200).send(getAllStreats);
+};
