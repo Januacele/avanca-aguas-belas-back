d101342bdfeb00d0e827eefa5597306ceadd96c4
Januacele <januacelevieira@hotmail.com>
2022-10-04 22:42:26 -0300

    fix: Adjust controller beauty

--- src/controllers/districtsController.ts
+++ src/controllers/districtsController.ts
@@ -7,13 +7,13 @@ export async function creatDistrictName(req: Request, res: Response) {
   const createDistrictName: IDistricts = req.body;
   await districtService.createDistrictName(createDistrictName);
   res.status(201).send("District added");
-}
+};
 
 
 export async function getDistrictName(req: Request, res: Response) {
   const getAllDistricts = await districtService.getDistrictsName();
   res.status(200).send(getAllDistricts);
-}
+};
 
 
 export async function getDistrictNameById(req: Request, res: Response) {
@@ -24,7 +24,7 @@ export async function getDistrictNameById(req: Request, res: Response) {
   }
   const getDistrictId = await districtService.getDistrictById(districtId);
   res.status(200).send(getDistrictId);
-}
+};
 
 
 export async function updateDistrictName(req: Request, res: Response) {
@@ -36,7 +36,7 @@ export async function updateDistrictName(req: Request, res: Response) {
   }
   const updateDistrict = await districtService.updateDistrictName(districtId, newDistrictName);
   res.status(200).send(updateDistrict);
-}
+};
 
 
 export async function deleteDistrictName(req: Request, res: Response) {
@@ -47,4 +47,4 @@ export async function deleteDistrictName(req: Request, res: Response) {
   }
   const deleteDistrict = await districtService.deleteDistrictName(districtId);
   res.status(200).send(deleteDistrict);
-}
+};
