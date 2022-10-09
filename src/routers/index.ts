791c814e4b95ff0a1fc0fec6568628b7170488c6
Januacele <januacelevieira@hotmail.com>
2022-10-09 13:47:36 -0300

    feat: index router: add streat and district information routes

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -2,12 +2,16 @@ import { Router } from "express";
 import authRouter from "./authRouter.js";
 import districtsRouter from "./districtsRouter.js";
 import streatRouter from "./streatsRouter.js";
-
+import streatInformationRouter from "../routers/streatsInformationRoute.js";
+import districtInformationRouter from "./districtInformationRouter.js";
 
 const router = Router();
 
 router.use(authRouter);
 router.use(districtsRouter);
 router.use(streatRouter);
+router.use(streatInformationRouter);
+router.use(districtInformationRouter);
+
 
 export default router;
