4b82a11fb0985c2b5cb8c004f06d4080e1760de0
Januacele <januacelevieira@hotmail.com>
2022-10-10 15:52:52 -0300

    feat: add factories functions for tests

--- tests/factories/authFactory.ts
+++ tests/factories/authFactory.ts
@@ -1,4 +1,5 @@
 import { faker } from "@faker-js/faker";
+import { prisma } from '../../src/database';
 
 export async function createNewUser() {
     const password = faker.internet.password();
@@ -12,3 +13,28 @@ export async function createNewUser() {
 
     return newUser;
 };
+
+export async function createNewUserUnitTest(){
+    const password = faker.internet.password();
+    const newUser = {
+        name: faker.name.firstName(),
+        cpf: "222.111.111-55",
+        email: faker.internet.email(),
+        password,
+        secretary: faker.word.noun(),
+        createdAt: faker.date.recent(),
+        updatedAt: faker.date.recent()
+    }
+
+    return newUser;
+}
+
+export async function registeredUserEmail(){
+    const originalUser = await createNewUserUnitTest();
+    const name = originalUser.email;
+    await prisma.user.create({
+        data: originalUser,
+    });
+
+    return name;
+}
